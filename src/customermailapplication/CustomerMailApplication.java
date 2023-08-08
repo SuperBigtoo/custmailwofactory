f129021ad36aad80b2f501081c156ff5335dd03a
Phakorn Pasawast <75010303+SuperBigtoo@users.noreply.github.com>
2023-08-08 16:01:52 +0700

    Add files via upload

--- src/customermailapplication/CustomerMailApplication.java
+++ src/customermailapplication/CustomerMailApplication.java
@@ -19,18 +19,9 @@ public class CustomerMailApplication {
      */
     private Customer customer;
     public void getCustomerTypeFromUser(String customerType) {
-        switch(customerType) {
-            case "Regular":
-                customer = new RegularCustomer();
-                break;
-            case "Mountain":
-                customer = new MountainCustomer();
-                break;
-            case "Delinquent":
-                customer = new DelinquentCustomer();
-                break;
-        }
+        customer = CustomerFactory.createCustomer(customerType);
     }
+    
     public String generateMail() {
         return customer.createMail();
     }
@@ -50,7 +41,6 @@ public class CustomerMailApplication {
             case 3:
                 app.getCustomerTypeFromUser("Delinquent");
                 break;
-            
         }
         System.out.println(app.generateMail());        
     }
